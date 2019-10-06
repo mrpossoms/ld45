031b8541bfc3bc52986edaf5ccf14d570afc1273
Kirk Roerig <mr.possoms@gmail.com>
2019-10-06 12:27:01 -0700

    Nearest debris indicator

--- game.server.js
+++ game.server.js
@@ -20,12 +20,27 @@ function gravitational_force(position, body_position, body_mass)
 	return r.norm().mul(m / r_2);
 }
 
-
-function asteroid_dynamics(asteroids, dt)
+function debris_cleanup(debris)
 {
-	for (var i = 0; i < asteroids.length; i++)
+  var list = [];
+
+  for (var i = 0; i < debris.length; i++)
+  {
+    var a = debris[i];
+    if (a.position.len() > 20)
+    {
+      list.push(a);
+    }
+  }
+
+  return list;
+}
+
+function debris_dynamics(debris, dt)
+{
+	for (var i = 0; i < debris.length; i++)
 	{
-		var a = asteroids[i];
+		var a = debris[i];
 		var previousPosition = a.position;
 		var velocity;
 
@@ -33,10 +48,12 @@ function asteroid_dynamics(asteroids, dt)
 		{
 			var force = gravitational_force(a.position, [0, 0, 0], k.moon.mass);
 			const do_n_body = false;
-			for (var j = 0; do_n_body && j < asteroids.length; j++)
+
+      // n-body calculation
+			for (var j = 0; do_n_body && j < debris.length; j++)
 			{
 				if (j == i) { continue; }
-				const b = asteroids[j];
+				const b = debris[j];
 				const grav = gravitational_force(a.position, b.position, b.mass);
 
 				force = force.add();
@@ -52,7 +69,7 @@ function asteroid_dynamics(asteroids, dt)
 			a.position = player.state.position.add(player.forward());
 			velocity = player.state.velocity;
 		}
-		// TODO: Add ray code with previous position and velocity and asteroid player to check if they scored
+		// TODO: Add ray code with previous position and velocity and debris player to check if they scored
 	}
 }
 
@@ -61,7 +78,7 @@ module.exports.server = {
   players: {},
   // complete game state
   state: {
-    asteroids: []
+    debris: []
   },
   // handlers for all player connection events
   player: {
@@ -132,6 +149,21 @@ module.exports.server = {
       const f_acc = player.forward().mul(t[2]);
       const acc = r_acc.add(u_acc).add(f_acc);
 
+      // player debris collision
+      for (var i = 0; i < this.state.debris.length; i++)
+      {
+        var d = this.state.debris[i];
+        const r = d.mass;
+
+        if (player.state.position.sub(d.position).len() < r)
+        {
+          console.log(player_key + 'collided with debris ' + i);
+          const tmp = player.state.velocity;
+          player.state.velocity = d.velocity;
+          d.velocity = tmp;
+        }
+      }
+
       // accelerate player
       player.state.velocity = player.state.velocity.add(acc.mul(dt));
 
@@ -141,28 +173,28 @@ module.exports.server = {
       player.state.position = player.state.position.add(player.state.velocity);
       this.player.update(player, dt);
 
-      // update state of asteroid if captured by player
+      // update state of debris if captured by player
       if (player.state.action !== undefined) {
-        if (player.state.asteroidId !== undefined) {
-          // throw asteroid
-          this.state.asteroids[player.state.asteroidId].captured = false;
-          this.state.asteroids[
-            player.state.asteroidId
+        if (player.state.debrisId !== undefined) {
+          // throw debris
+          this.state.debris[player.state.debrisId].captured = false;
+          this.state.debris[
+            player.state.debrisId
           ].velocity = player.velocity.add(player.forward().mul(0.33));
-          delete player.state.asteroidId;
+          delete player.state.debrisId;
         } else {
-          // grab asteroid
-          for (var i = 0; this.state.asteroids; i++) {
+          // grab debris
+          for (var i = 0; this.state.debris; i++) {
             if (
               player.state.position
                 .add(player.forward())
-                .sub(this.state.asteroids[i].position)
+                .sub(this.state.debris[i].position)
                 .len() <= 1 &&
-              !this.state.asteroids[i].captured
+              !this.state.debris[i].captured
             ) {
-              player.state.asteroidId = i;
-              this.state.asteroids[i].captured = true;
-              this.state.asteroids[i].player = player;
+              player.state.debrisId = i;
+              this.state.debris[i].captured = true;
+              this.state.debris[i].player = player;
             }
           }
         }
@@ -172,8 +204,11 @@ module.exports.server = {
       this.state.players[player_key] = player.state;
     }
 
-    // update position of asteroids and if they would go through the gate
-    asteroid_dynamics(this.state.asteroids, dt);
+    // remove debris that have fallen into the moon
+    this.state.debris = debris_cleanup(this.state.debris);
+
+    // update position of debris and if they would go through the gate
+    debris_dynamics(this.state.debris, dt);
 
     // send states to all players
     for (var player_key in this.players) {
@@ -183,20 +218,20 @@ module.exports.server = {
     }
   },
   //initial state
-  setup: function(asteroidCount) {
+  setup: function(debrisCount) {
     this.state.gate = { position: [0, 0, 0] };
-    for (var i = 0; i < asteroidCount; i++) {
+    for (var i = 0; i < debrisCount; i++) {
       const r = Math.random() * 50 + 50;
       const t = Math.random() * Math.PI * 2;
       const p = [ Math.cos(t) * r, Math.random() * 10 - 5, Math.sin(t) * r ];
       const v = tangent(p, [0, 0, 0]).mul(Math.sqrt(k.moon.mass / r));
-      var asteroid = {
+      var debris = {
         level: 0,
         mass: 0.25 + Math.random(),
         position: p,
         velocity: v
       };
-      this.state.asteroids.push(asteroid);
+      this.state.debris.push(debris);
     }
   }
 };
